a3855b821bd71a6b3020b85b299b345423eb7505
Davi Mello <davimello1994@gmail.com>
2019-09-30 22:15:09 -0400

    HTML and CSS

--- server.js
+++ server.js
@@ -3,24 +3,22 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var session = require("express-session");
 var passport = require("./config/passport");
+var multer = require("multer");
 
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+const DIR = "./public/uploads";
 
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
-
-
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(__dirname + '/public'));
-
-
 // Handlebars
 app.engine(
   "handlebars",
@@ -29,19 +27,15 @@ app.engine(
   })
 );
 app.set("view engine", "handlebars");
-
 // Routes
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
-
 var syncOptions = { force: false };
-
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
-
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
   app.listen(PORT, function() {
@@ -52,5 +46,4 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
-
 module.exports = app;
